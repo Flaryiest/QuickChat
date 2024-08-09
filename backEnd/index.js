4fedf5d34259a74049fa2de2b21f6ba2b260cc44
Flaryiest <ericmzuo1@gmail.com>
2024-08-09 11:48:39 -0600

    backend setup

---
+++ backEnd/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const apiRouter = require("./routes/apiRouter.js")
+const mainRouter = require("./routes/mainRouter.js")
+const port = process.env.PORT || 3000
+
+
+app.use(cors())
+app.use(express.json())
+app.get("/", (req, res) => {
+    console.log("test")
+    return res.send("Recieved Request")
+})
+app.use("/api", apiRouter)
+app.use("/", mainRouter)
+
+app.listen(port, () => {
+    console.log("Listening on port: " + String(port))
+})
---
+++ backEnd/routes/mainRouter.js
@@ -0,0 +1,4 @@
+const express = require("express")
+const mainRouter = express.Router()
+
+module.exports = mainRouter
